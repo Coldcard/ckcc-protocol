54da76851542c3298566b0865c9e8b6119198c68
Peter D. Gray <peter@conalgo.com>
2022-02-28 09:20:57 -0500

    new version

--- ckcc/__init__.py
+++ ckcc/__init__.py
@@ -1,6 +1,6 @@
 # (c) Copyright 2021 by Coinkite Inc. This file is covered by license found in COPYING-CC.
 
-__version__ = '1.2.1'
+__version__ = '1.3.0'
 
 __all__ = [ "client", "protocol", "constants" ]
 

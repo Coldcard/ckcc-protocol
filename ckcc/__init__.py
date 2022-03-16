d302f01be565f6d317a1c5cbe5f299b812f8aa9c
Peter D. Gray <peter@conalgo.com>
2022-03-16 08:54:27 -0400

    bump

--- ckcc/__init__.py
+++ ckcc/__init__.py
@@ -1,6 +1,6 @@
 # (c) Copyright 2021 by Coinkite Inc. This file is covered by license found in COPYING-CC.
 
-__version__ = '1.3.0'
+__version__ = '1.3.1'
 
 __all__ = [ "client", "protocol", "constants" ]
 

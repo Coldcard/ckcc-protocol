757209900a90a7f834fb6a60b38cc36ba456f486
Bryan Bishop <kanzure@gmail.com>
2022-06-29 15:24:57 -0500

    fix typo in CCProtocolPacker
    from https://github.com/bitcoin-core/HWI/pull/616

--- ckcc/protocol.py
+++ ckcc/protocol.py
@@ -64,7 +64,7 @@ class CCProtocolPacker:
 
     @staticmethod
     def start_backup():
-        # prompts user with password for encrytped backup
+        # prompts user with password for encrypted backup
         return b'back'
 
     @staticmethod
